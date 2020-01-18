9f5f9c7dfe19273b77c791c3d75e45a1f42df87e
Sagar Patra <pydevsp@gmail.com>
2020-01-18 13:53:36 +0530

    create numeric system in py

---
+++ PYTHON_3.8/datatype_int.py
@@ -0,0 +1,39 @@
+#1. decimal form number == base_10
+a=150
+print(a ,"..'decimal'..") #integral data type
+
+print("====")
+
+#2. binary form number == base_2  ,0 & 1 are allowed ,prefixed with 0b or' 0B ,any number convert in binary no.== bin()  .
+b=11
+c=10
+d=101111000
+aa=0b1111 # convert binary to decimal number
+print(bin(45)) #convert decimal to binary number
+print(b,"..'binary'..")
+print(c)
+print(d)
+print(aa ,"..'binary to decimal'..")
+
+print('=====')
+
+#3. octa-decimal number  == base_8 , 0 to 7 are allowed , prefixed with 0o or' 0O ,any number convert in octa no.== oct().
+e=1542
+ee=0o11754  # it is binaryocta-decimal number 
+print(oct(451))  #convert decimal to octa-decimal number
+print(e)
+print(ee ,"..'defult  decimal value print'..")
+
+print('=====')
+
+#4.hexa-deccimal number  == base_16  , 0 to 9 & a to f are allowed , prefixed with 0x or' 0X ,any number convert in hexa no.== hex().
+x=('0x4578ad475','it is hexa number')
+y=0X1542faa44
+print(hex(468464))
+print(x)
+print(y ,"..'defult  decimal value print'..")
+
+
+ 
+
+
