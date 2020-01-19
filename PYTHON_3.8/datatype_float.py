dad01b404ae6adc3a8af02488cae90aeb7f42b7f
Sagar Patra <pydevsp@gmail.com>
2020-01-19 18:44:19 +0530

    float numbers intro

---
+++ PYTHON_3.8/datatype_float.py
@@ -0,0 +1,14 @@
+aa=154.00
+print(type(aa))
+
+bb=10.021           # float values only using decimal form .
+print(type(bb),(bb))
+
+cc=aa+bb
+print(cc)
+
+#dd=0o123.56 
+#print(type(dd))  
+#    dd=0o123.56 
+#             ^
+#SyntaxError: invalid syntax
