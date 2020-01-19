d9c88abb558236b042faf37dc434a25281ab5e72
Sagar Patra <pydevsp@gmail.com>
2020-01-19 18:43:27 +0530

    complex numbers intro

---
+++ PYTHON_3.8/datatype_complex.py
@@ -0,0 +1,17 @@
+# complex number is of the form  ==    a+bj ==> "a" real value & "bj" imaginary value 
+
+a=3+5j
+b=3.8+7j
+c=5.0-2j
+d=88+5.5j
+B=22+3.124j 
+ab=a+b
+cd=c+d
+SA=a-b
+print(type(a),(a))
+print(type(b),(b))
+print(type(c),(c))
+print((B),type(B))
+print(ab)
+print(cd)
+print(SA)
