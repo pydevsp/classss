d0e1056fc5930b1eadcd745dafdc114100abcfec
Sagar Patra <pydevsp@gmail.com>
2020-02-07 02:12:47 +0530

    jupyter notebook file

---
+++ PYTHON_3.8/python_class/Operators.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # operators ::
+
+# ## Arithmetic Operator:-
+#  
+
+# ###  + , -  , *  , ** [Power operator], / , // [Floor Division], % [Modulo]
+
+# In[2]:
+
+
+a=10
+b=2
+print(a)
+print(b)
+
+
+# In[3]:
+
+
+a+b
+
+
+# In[4]:
+
+
+a-b
+
+
+# In[5]:
+
+
+a*b
+
+
+# In[6]:
+
+
+a/b
+
+
+# In[7]:
+
+
+b/a
+
+
+# In[13]:
+
+
+A=10
+B=2
+print(A)
+print(B)
+
+
+# In Python applications, * operator will be used to perform Arithmetic Multiplication operation. 
+# 
+# In Python applications, ** operator is representing power operator.
+
+# In[14]:
+
+
+print(A*B)
+
+
+# In[15]:
+
+
+print(A**B)
+
+
+# In Python applications, / operator will perform Arithmetic division operation and it will return always float value 
+# as result whether the operands are int or float.
+# 
+# In Python applications, // operator will perform Arithmetic Division operator and it will return the results 
+# as per the following conditions.
+# 
+# 1. If both the operands are int type then the result of // operator is int only.
+# 2. If either of the operands or both the operands are float then the result of //      operator is float.
+
+# In[16]:
+
+
+print(A/B)
+
+
+# In[17]:
+
+
+print(A//B)
+
+
+# In[18]:
+
+
+x=15.0
+y=5
+print(x/y)
+
+
+# In[19]:
+
+
+print(x//y)
+
+
+# In[20]:
+
+
+x=12.1
+y=2.1
+
+print(x/y)
+
+
+# In[21]:
+
+
+print(x//y)
+
+
+# ## Assignment Operator :-
+
+# - =
+# - +=
+# - -=
+# - *=
+# - /=
+# - %=
+# - **= 
+# - //=
+
+# In[41]:
+
+
+a=5
+
+a
+
+
+# In[42]:
+
+
+a+=5
+print(a)
+a
+
+
+# In[43]:
+
+
+a-=2
+print(a)
+
+
+# In[44]:
+
+
+a*=5
+print(a)
+
+
+# In[45]:
+
+
+a/=8
+print(a)
+
+
+# In[46]:
+
+
+a**=2
+print(a)
+
+
+# In[47]:
+
+
+a//= 4
+print(a)
+
+
+# In[ ]:
+
+
+
+
