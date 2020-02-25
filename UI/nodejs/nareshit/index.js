21b05e5b78fdf8c5c3a7993bcaf0beefadac24b8
Sagar Patra <pydevsp@gmail.com>
2020-02-25 15:41:47 +0530

    # node js # //connect to db  ==> add

--- UI/nodejs/nareshit/index.js
+++ UI/nodejs/nareshit/index.js
@@ -1,7 +1,7 @@
 // load "express" modules
 var express = require("express");    // require == import
 var monogooes = require("mongoose") ;
-// var expressHandlebars = require("express-Handlebars") ;
+var expressHandlebars = require("express-handlebars") ;
 
 // crreate server 
 var app = express() ;  // store module
@@ -14,51 +14,51 @@ var courseSchema = new monogooes.Schema ({
 var courseModel = monogooes.model ("courses" , courseSchema);
 
 //set the "handelbars as view engine"
-//app.engine ("hbs" , expressHandlebars()) ;
-// app.set ("view engine", "hbs") ;
+app.engine ("hbs" , expressHandlebars()) ;
+app.set ("view engine", "hbs") ;
 
 // start listener server
-app.listen(6070) ;
+app.listen(6070) ;        // 1024 to 65535  proto no.
 console.log("server started at port 6070");
 
 app.get("/home" , function(req ,res) {
-    // monogooes.connect ("mongodb ://localhost/naresgit") ;  //connect to db
+    res.sendfile(__dirname + "/home.html");
+});
+app.get ("/courses" , function(req , res)
+{
+    monogooes.connect ("mongodb ://localhost/nareshit") ;  //connect to db
+
+
 
     //find(retrive data) from database
     courseModel.find(function(err,data)
     {
-        if (err = null)
-        {
-            //success
+                //connect to db
+        mongoose.connect("mongodb://localhost/nareshit" , {
+            useNewUrlParser:true , useUnifiedTopology :true
+        });
+
+        //find (retrieve data) from database
+        CourseModel.find(function(err, data) {
+            if (err == null)
+            {
+            //success (data loaded from db)
             console.log(data);
-            res.render("courses.hbs" , {courses : data , layout :false}) ;
-            //res.send(daa);
-        }
-        else
-        {
-            //failure(data not loaded)
+            res.render("courses.hbs", { courses: data, layout: false } );
+            //res.send(data);
+            }
+            else
+            {
+            //failure (data not loaded)
             console.log(err);
-            res.send("Error")
-        }
-        monogooes.connection.close() ;});
-    
+            res.send("Error");
+            }
+            
+            mongoose.connection.close();  
+        });
     });
-    
-    
-    
-    
-    console.log("Request received at /home") ;
-    // res.send("Helo from server") ;
-    res.sendfile(__dirname + "/home.html") ;
 
-}) ;
 
-// 
-app.get("/course" , function (req , res){
-    res.sendfile(__dirname + "/courses.html") ;
-}) ;
-
-//
 app.get ("/contact" , function (req, res ){
     res.sendfile(__dirname + "/contact.html");        // __dirnamme = current path file location
 }) ;
