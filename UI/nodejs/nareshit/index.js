8e2fda9182cf158e401cda6ebd19675644df9afc
Sagar Patra <pydevsp@gmail.com>
2020-02-28 01:24:42 +0530

    # node js  &  mongo db
    # update   delete , edit

--- UI/nodejs/nareshit/index.js
+++ UI/nodejs/nareshit/index.js
@@ -1,65 +1,206 @@
-// load "express" modules
-var express = require("express");    // require == import
-var monogooes = require("mongoose") ;
-var expressHandlebars = require("express-handlebars") ;
+//load modules
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyparser = require("body-parser");
+var cookieparser = require("cookie-parser");
+var expresssession = require("express-session");
 
-// crreate server 
-var app = express() ;  // store module
+//create server
+var app = express();
 
-//create schema 
-var courseSchema = new monogooes.Schema ({
-    courseno :Number , coursename : String , duretion : String 
-}) ;
-// create model 
-var courseModel = monogooes.model ("courses" , courseSchema);
+//create schema
+var CourseSchema= new mongoose.Schema( { courseno: Number, coursename: String, duration: String }, { versionKey: false } );
 
-//set the "handelbars as view engine"
-app.engine ("hbs" , expressHandlebars()) ;
-app.set ("view engine", "hbs") ;
+//create model
+var CourseModel = mongoose.model("courses", CourseSchema);
 
-// start listener server
-app.listen(6070) ;        // 1024 to 65535  proto no.
-console.log("server started at port 6070");
+//Set the "handlebars" as view engine
+app.engine("hbs", expressHandlebars());
+app.set("view engine", "hbs");
 
-app.get("/home" , function(req ,res) {
-    res.sendfile(__dirname + "/home.html");
+//enable body parser
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+//enable cookies
+app.use(cookieparser());
+
+//enable session
+app.use(expresssession({ secret: "riji5953tj45", cookie: { maxAge: 1000 * 60 * 10 } }));
+
+//start listener (server)
+app.listen(6070); //1024 to 65535
+console.log("Server started at port 6070");
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/home.html");
 });
-app.get ("/courses" , function(req , res)
-{
-    monogooes.connect ("mongodb ://localhost/nareshit") ;  //connect to db
 
+app.get("/home", function(req, res) {
+  res.sendFile(__dirname + "/home.html");
+});
 
+app.get("/courses", function(req, res) {
+  //connect to db
+  mongoose.connect("mongodb://localhost/nareshit", { useNewUrlParser: true,useUnifiedTopology: true });
 
-    //find(retrive data) from database
-    courseModel.find(function(err,data)
+  //find (retrieve data) from database
+  CourseModel.find(function(err, data) {
+    if (err == null)
     {
-                //connect to db
-        mongoose.connect("mongodb://localhost/nareshit" , {
-            useNewUrlParser:true , useUnifiedTopology :true
-        });
+      //success (data loaded from db)
+      console.log(data);
+      
+      res.render("courses.hbs", { courses: data, currentCourse: req.session.currentCourseName, layout: false } );
+      //res.send(data);
+    }
+    else
+    {
+      //failure (data not loaded)
+      console.log(err);
+      res.send("Error");
+    }
+    
+    mongoose.connection.close();  
+  });
+});
 
-        //find (retrieve data) from database
-        CourseModel.find(function(err, data) {
-            if (err == null)
-            {
-            //success (data loaded from db)
-            console.log(data);
-            res.render("courses.hbs", { courses: data, layout: false } );
-            //res.send(data);
-            }
-            else
-            {
-            //failure (data not loaded)
-            console.log(err);
-            res.send("Error");
-            }
-            
-            mongoose.connection.close();  
-        });
-    });
+app.get("/contact", function(req, res) {
+  res.sendFile(__dirname + "/contact.html");
+});
 
 
-app.get ("/contact" , function (req, res ){
-    res.sendfile(__dirname + "/contact.html");        // __dirnamme = current path file location
-}) ;
+//Code inserting new course [Executes when the user clicks on 'New Course' hyperlilnk]
+app.get("/new-course", function(req, res) {
+  res.render("new-course.hbs", { layout: false });
+});
 
+//Executes when the user clicks on 'Save' button 'New Course' page
+app.post("/insert-course", function(req, res) {
+  //connect to db
+  mongoose.connect("mongodb://localhost/nareshit", { useNewUrlParser: true, useUnifiedTopology: true });
+
+  var newCourse = new CourseModel({
+    courseno: req.body.courseno,
+    coursename: req.body.coursename,
+    duration: req.body.duration });
+  
+  newCourse.save(function(err)
+  {
+    if (err == null)
+    {
+      //successfully inserted
+      req.session.currentCourseName = req.body.coursename;
+      res.redirect("/courses");
+    }
+    else
+    {
+      //inserting failed
+      res.send("Failed");
+    }
+
+    mongoose.connection.close();
+  });
+});
+
+
+//Update course
+app.get("/edit-course", function(req, res) {
+  //fetch course data from databse
+  var cno = req.query.courseno;
+
+  mongoose.connect("mongodb://localhost/nareshit", { useNewUrlParser: true, useUnifiedTopology: true });
+
+  CourseModel.findOne({ courseno: cno }, function(err, data) {
+    if (err == null)
+    {
+      console.log(data);
+      res.render("edit-course.hbs", { course: data, courseNotFound: data == null, layout: false });
+    }
+    else
+    {
+      console.log(err);
+      res.send("Error");
+    }
+    mongoose.connection.close();
+  });
+});
+
+
+//Update-course
+app.post("/update-course", function(req, res) {
+  mongoose.connect("mongodb://localhost/nareshit", { useNewUrlParser: true, useUnifiedTopology: true });
+
+  var cno = req.body.courseno;
+
+  CourseModel.findOne({ courseno: cno}, function(err, data) {
+    if (err == null)
+    {
+      //Course data is retrieved from database. Now we have to update
+      data.coursename = req.body.coursename;
+      data.duration = req.body.duration;
+
+      data.save(function(err) {
+        if (err == null)
+        {
+          res.redirect("/courses");
+        }
+        else
+        {
+          console.log(err);
+          res.send("Failed");
+        }
+
+        mongoose.connection.close();
+      });
+    }
+    else
+    {
+      console.log(err);
+      res.send("Failed to fetch data from database");
+      mongoose.connection.close();
+    }
+  });
+
+});
+
+
+
+//Delete course
+app.get("/delete-course", function(req, res) {
+  var cno = req.query.courseno;
+
+  mongoose.connect("mongodb://localhost/nareshit", { useNewUrlParser: true, useUnifiedTopology: true });
+
+  CourseModel.findOne({ courseno: cno}, function(err, data) {
+    if (err == null)
+    {
+      res.render("delete-course.hbs", { course: data, courseNotFound: data == null, layout: false });
+    }
+    else
+    {
+      console.log(err);
+      res.send("Error");
+    }
+    mongoose.connection.close();
+  });
+});
+
+
+app.post("/delete-course", function(req, res) {
+  mongoose.connect("mongodb://localhost/nareshit", { useNewUrlParser: true, useUnifiedTopology: true });
+
+  CourseModel.remove({ courseno: req.body.courseno }, function(err) {
+    if (err == null)
+    {
+      res.redirect("/courses");
+    }
+    else
+    {
+      console.log(err);
+      res.send("Failed to delete");
+    }
+    mongoose.connection.close();
+  })
+});
