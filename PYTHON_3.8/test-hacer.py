e72e39aebc9a097c84ff82c51f255d64be4c339d
Sagar Patra <pydevsp@gmail.com>
2020-03-05 02:31:45 +0530

    # HACHERRANK Q.

---
+++ PYTHON_3.8/test-hacer.py
@@ -0,0 +1,29 @@
+# def is_leap(year):
+#     leap = False
+#     n = year
+#     if n % 400 == 0:
+#         return True
+#     if n % 100 == 0:
+#         return False
+#     if n % 4 == 0:
+#         return True
+#     return False
+#     return leap
+
+# year = int(input())
+# print(is_leap(year))
+
+
+N = int(input("value :"))
+for x in range(1,N+1):
+    print(x ,end='')
+
+
+
+# def add(a,b):
+#  print(a+b)
+
+
+# add(10,20)
+
+# print(add(45,50))
